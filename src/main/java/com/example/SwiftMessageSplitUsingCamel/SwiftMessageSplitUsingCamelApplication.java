9d3956f566b72e21e4059d809fda9995b465e445
prosanna <prosanna2005@gmail.com>
2021-11-13 22:10:22 +0530

    Initial version.

---
+++ src/main/java/com/example/SwiftMessageSplitUsingCamel/SwiftMessageSplitUsingCamelApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwiftMessageSplitUsingCamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftMessageSplitUsingCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftMessageSplitUsingCamelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SwiftMessageSplitUsingCamel/SwiftMessageSplitUsingCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SwiftMessageSplitUsingCamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftMessageSplitUsingCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
